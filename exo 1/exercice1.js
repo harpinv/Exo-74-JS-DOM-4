f6c87bbe627f1167bebf909ca49f2fbd6267ebf0
vince <vincent.harpin@orange.fr>
2022-11-22 21:49:24 +0100

    exercice 74

--- exo 1/exercice1.js
+++ exo 1/exercice1.js
@@ -1,6 +1,11 @@
 //Votre code ci dessous
 
+let photo = document.getElementById('cat-pic');
 
+photo.addEventListener("click", function () {
+    let pique = document.getElementById('cat-chat');
+    pique.innerText = 'Miaou';
+})
 
 
 
--- exo 3/exercice.js
+++ exo 3/exercice.js
@@ -1,9 +1,12 @@
 let libButton = document.getElementById('lib-button');
-let libIt = function() {
-    let storyDiv = document.getElementById("story");
-    storyDiv.innerHTML = "??Your story here";
-};
-libButton.addEventListener('click', libIt);
+let storyDiv = document.getElementById('story');
+let noun = document.getElementById('noun');
+let adjective = document.getElementById('adjective');
+let person = document.getElementById('person');
+
+libButton.addEventListener('click', function() {
+    storyDiv.innerText = "La " + noun.value + " est " + adjective.value + " dans un " + person.value + ".";
+})
 
 
 
